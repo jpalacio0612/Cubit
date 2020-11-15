8e390fefc23680401dec474b9731fbf36fdbfdf3
Jonathan Palacio <jpalacio0612@gmail.com>
2020-11-15 15:54:09 -0500

    react-router-dom and Header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { Header } from './components/Header'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { useHistory } from 'react-router-dom'
+import { useStyles } from './SyledHeader'
+
+export const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const history = useHistory()
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = (param) => {
+    history.push(param)
+    setAnchorEl(null)
+  }
+
+  const classes = useStyles()
+  return (
+    <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            className={classes.title}
+            onClick={() => history.push('/')}
+          >
+            Cubit
+          </Typography>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => handleClose('/')}>Home</MenuItem>
+        <MenuItem onClick={() => handleClose('/questions')}>Preguntas</MenuItem>
+      </Menu>
+    </div>
+  )
+}
